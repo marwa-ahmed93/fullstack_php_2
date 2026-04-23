98e5687cc82cf97fff837896c6312881981796af
marwa-ahmed93 <https://github.com/marwa-ahmed93/Restaurant.git>
2026-04-23 14:15:24 +0200

    php_5

---
+++ session_5php/index.php
@@ -0,0 +1,86 @@
+<?php
+
+
+// echo "hello worl";
+// echo "hello worl";
+// echo "hello worl";
+// echo "hello worl";
+// echo "hello worl";
+
+// echo
+// 10+6 5+5 ;
+// variables  
+
+//  $name = "marwa";      //losly datatype
+//  $num = 5;  
+//  $num = "sara";
+//  $num2 = 5.3;
+// $is_admin = true ;
+
+// $start;
+
+// echo gettype( $start );
+
+// echo "5"+5;  
+// echo "<br>";
+// echo (int) "marwa" +5;  //0 +5 =>5
+
+// echo "<br>";
+
+// echo  (int)  10.5 + 10 ;
+
+
+//if 
+
+// $age =10 ;
+
+// if($age  >15){
+//     echo "you can register";
+// }
+// elseif($age > 11){
+//         echo "you can`t register";
+
+// }
+// else{
+//     echo "not valid";
+// }
+
+
+$age = 20 ;
+$gender = 'male';
+
+if($age >= 20){
+   if($gender == "male"){
+    echo "success";
+   }
+}
+else{
+    echo 'false';
+}
+
+// if(" "){  //true
+//     echo "welcome";
+// }
+/**
+ * = 
+ * ==     value
+ * === value datatype 
+ */
+// $num = '10';
+// // echo gettype($num);  //string
+// if($num !== 10){    
+//     echo "done";
+// }
+
+$number = 10;
+//  if($number <9){
+//     echo "good";
+//  }
+//  else{
+//     echo "no";
+//  }
+
+ echo $number < 9 ? "good" : "no" ;
+  
+
+?>
